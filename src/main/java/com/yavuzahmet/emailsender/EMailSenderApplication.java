bcdd62382fb9b0d749fdb92424c29afddb033d80
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-02-22 13:22:59 +0300

    Initial commit

---
+++ src/main/java/com/yavuzahmet/emailsender/EMailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.yavuzahmet.emailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMailSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMailSenderApplication.class, args);
+    }
+
+}
